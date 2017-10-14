aef1bf876773718be1e02e1da1b2f323db78c7db
David Chambers <dc@davidchambers.me>
2017-10-14 18:09:54 +0200

    fantasy-land@3.4.0

---
+++ test/Sum.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var FL = require('fantasy-land');
+
+var Z = require('..');
+
+
+//  Sum :: Number -> Sum
+function Sum(value) {
+  if (!(this instanceof Sum)) return new Sum(value);
+  this.value = value;
+}
+
+Sum['@@type'] = 'sanctuary-type-classes/Sum';
+
+Sum[FL.empty] = function() { return Sum(0); };
+
+Sum.prototype[FL.equals] = function(other) {
+  return Z.equals(this.value, other.value);
+};
+
+Sum.prototype[FL.concat] = function(other) {
+  return Sum(this.value + other.value);
+};
+
+Sum.prototype[FL.invert] = function() {
+  return Sum(-this.value);
+};
+
+Sum.prototype.inspect =
+Sum.prototype.toString = function() {
+  return 'Sum(' + Z.toString(this.value) + ')';
+};
+
+module.exports = Sum;
