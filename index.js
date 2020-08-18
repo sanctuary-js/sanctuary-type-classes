5224705d8f53fbfdbf794bcc780192ad6935f4f6
David Chambers <dc@davidchambers.me>
2020-08-18 18:07:13 +0200

    remove Function.name polyfill

--- index.js
+++ index.js
@@ -254,15 +254,6 @@
     return _funcPath (false, path, implementations);
   }
 
-  //  functionName :: Function -> String
-  var functionName = has ('name', function f() {}) ?
-    function functionName(f) { return f.name; } :
-    /* istanbul ignore next */
-    function functionName(f) {
-      var match = /function (\w*)/.exec (f);
-      return match == null ? '' : match[1];
-    };
-
   //  $ :: (String, Array TypeClass, StrMap (Array Location)) -> TypeClass
   function $(_name, dependencies, requirements) {
     function getBoundMethod(_name) {
@@ -271,7 +262,7 @@
         function(typeRep) {
           var f = funcPath ([name], typeRep);
           return f == null && typeof typeRep === 'function' ?
-            implPath ([functionName (typeRep), name]) :
+            implPath ([typeRep.name, name]) :
             f;
         } :
         function(x) {
